4a2b691e40e4c91bd45ebf82b65511b4974adc62
mm975000 <mepower78@yahoo.co.uk>
2015-12-18 18:30:15 +0300

    s

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set("view engine","html");
 app.set("views",__dirname+"/views");
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use("http://mm2mm.azurewebsites.net/favicon.ico",express.static(path.join(__dirname,"/public/favicon.ico")));
+//app.use("http://mm2mm.azurewebsites.net/favicon.ico",express.static(path.join(__dirname,"/public/favicon.ico")));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,6 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",function(req,res){
   res.render("index",{"title":"nf1"});
-  
+  res.end();
 
 });
