06e2fe55dd2e4276ab1bc4961b1f7c29c1131041
mm975000 <mepower78@yahoo.co.uk>
2015-12-18 18:06:29 +0300

    s2

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path=require("path");
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var cons=require("consolidate");
+var swig=require("swig");
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.engine("html",cons.swig);
+app.set("view engine","html");
+app.set("views",__dirname+"/views");
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use("http://mm2mm.azurewebsites.net/favicon.ico",express.static(path.join(__dirname,"/public/favicon.ico")));
@@ -23,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",function(req,res){
-  res.send("Hello World");
-  res.end();
+  res.render("index",{"title":"nf1"});
+  
 
 });
